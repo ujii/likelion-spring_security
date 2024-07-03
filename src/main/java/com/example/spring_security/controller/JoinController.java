86cbbb8445db1a78451d0e23829aee0574c4e03b
ujii <wcm9763@naver.com>
2024-07-04 02:15:43 +0900

    ✨ Update joinProcess Method

--- src/main/java/com/example/spring_security/controller/JoinController.java
+++ src/main/java/com/example/spring_security/controller/JoinController.java
@@ -1,13 +1,18 @@
 package com.example.spring_security.controller;
 
+import com.example.spring_security.dto.JoinDTO;
+import com.example.spring_security.service.JoinService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class JoinController {
 
-    // 회원가입 페이지로 이동
+    private final JoinService joinService;
+
     @GetMapping("/join")
     public String joinPage() {
         return "join";
@@ -15,7 +20,9 @@ public class JoinController {
 
     // 회원가입
     @PostMapping("/joinProc")
-    public String joinProcess() {
-        return "redirect:/login"; // 로그인 페이지로 리다이렉트
+    public String joinProcess(JoinDTO joinDTO) {
+        // 회원가입 로직 작성 필요
+        joinService.joinProcess(joinDTO); // 회원가입 수행
+        return "redirect:/login"; // 회원가입 성공시 로그인 페이지로 연결
     }
 }
