a97fb7159cc5e4f7220fb3a198165bcca3106822
ujii <wcm9763@naver.com>
2024-07-04 02:10:18 +0900

    ✨ Add UserEntity Class

---
+++ src/main/java/com/example/spring_security/entity/UserEntity.java
@@ -0,0 +1,22 @@
+package com.example.spring_security.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "USERENTITY")
+public class UserEntity {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @Column(unique = true) // 중복 방지용
+    private String username;
+    private String password;
+
+    private String role; // 사용자 권한 확인용
+}
