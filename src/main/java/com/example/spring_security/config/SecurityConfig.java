34bff410c66853c14b9ad4af73592653c6d1fbec
ujii <wcm9763@naver.com>
2024-07-04 02:07:31 +0900

    ✏️ Fix typos and annotations

--- src/main/java/com/example/spring_security/config/SecurityConfig.java
+++ src/main/java/com/example/spring_security/config/SecurityConfig.java
@@ -12,11 +12,10 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     // 비밀번호 암호화를 위한 빈 생성
-    @Bean // 빈으로 등록시킴으로서 어디서든 사용할 수 있게된다.
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
 
     // SecurityFilterChain 커스텀 빈 스프링 빈으로 등록
     @Bean
@@ -26,7 +25,7 @@ public class SecurityConfig {
         http
                 .authorizeHttpRequests(auth -> auth // 람다 식으로 작성 해야함
                         // 메인페이지와 로그인 페이지로의 접근은 모두 허용(인증 필요x)
-                        .requestMatchers("/", "/login","/join","joinProc").permitAll()
+                        .requestMatchers("/", "/login","/join","/joinProc").permitAll()
                         // admin 페이지로의 접근은 ADMIN 권한을 가진 경우에만 허용
                         .requestMatchers("/admin").hasRole("ADMIN")
                         // my 경로 이후의 모든 경로에 대해서 ADMIN, 혹은 USER권한 보유시 접근 가능
@@ -34,16 +33,12 @@ public class SecurityConfig {
                         // 위에서 설정하지 않은 모든 요청에 대해서는 인증(authenticated)된 사용자에 한해 허용(로그인 필요)
                         .anyRequest().authenticated()
                 )
-                // form 로그인 방식을 사용할 것이다.
-                .formLogin(auth -> auth
-                        .loginPage("/login") // 로그인은 /login에서 수행한다.
+                .formLogin(auth -> auth.loginPage("/login")
                         .loginProcessingUrl("/loginProc")
-                        .permitAll()
-                ) // 로그인 프로세싱은 해당 경로로 수행한다.
-
-                // 폼 로그인 방식을 사용하게 되면 csrf 설정이 디폴트로 동작하게 된다.
-                // csrf가 동작하면 POST 요청을 보낼 때 csrf 토큰도 함께 보내야 요청이 수행된다.
-                // 우리의 개발환경에서는 토큰ㅇ르 보내주도록 설정하지 않았기 때문에 임시로 비활성화 한다.
+                        .permitAll())
+                // csrf 설정 임시 비활성화
+                // 실제 서비스를 위해서는 활성화 시켜주어야한다.
+                // 이번 세션은 스프링 시큐리티가 무엇인지 학습하는 것이 목표였기 때문에 개발단계의 편의를 위해 비활성화 하였다.
                 .csrf(auth -> auth.disable());
 
         return http.build();
